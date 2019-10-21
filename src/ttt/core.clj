cd4466b2d41870017734154ccc8367e56a38f4a8
Angelica Gonzalez <angelica@8thlight.com>
2019-10-21 15:23:17 -0700

    Create board functions

--- src/ttt/core.clj
+++ src/ttt/core.clj
@@ -12,12 +12,36 @@
   [row]
   (clojure.string/join " | " row))
 
-(defn print-board [board]
+(defn print-board
+  [board]
   (let [size (math/sqrt (count board))]
     (let [rows (into [] (map add-padding (partition size board)))
           divider (str "\n" (clojure.string/join (repeat (count (first rows)) "-")) "\n")]
       (println (str "\n" (clojure.string/join divider rows))))))
 
+(defn place-marker
+  [board position marker]
+  (assoc board position marker))
+
+(defn position_available?
+  [board position]
+  (= (nth board position) position)) 
+
+(defn valid?
+  [position]
+  (if (some #(= position %) (initial-board)) 
+    true 
+    false))
+
+(defn valid-move? 
+  [board position]
+  (and (valid? position) (position_available? board position)))
+
+(defn make-move
+  [board position marker]
+  (if (valid-move? board position)
+    (place-marker board position marker)
+    (println "Invalid move")))
 
 ;====Player=====
 (defn player 
@@ -30,17 +54,22 @@
   [[player1 player2]]
   player1)
 
-;====UI=====
-(defn start-game-message
+(defn get-player-move
   []
-  (println "Let's play tic-tac-toe.")
-  (first-player-message players)
-  (println "Enter a number between 0-8 to select a spot."))
+  (read-string (read-line)))
+
+
+;====UI=====
 
 (defn first-player-message
   [[player1 player2]]
   (println "Player 1 is" (:marker player1)))
 
+(defn start-game-message
+  []
+  (println "Let's play tic-tac-toe.")
+  (first-player-message players)
+  (println "Enter a number between 0-8 to select a spot."))
 
 ;====Game====
 (defn start-game
