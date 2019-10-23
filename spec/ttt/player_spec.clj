6c33cee60d9e8a2a26efe55c8e0501719af5df06
Angelica Gonzalez <angelica@8thlight.com>
2019-10-23 11:03:59 -0700

    Add switch players function

--- spec/ttt/player_spec.clj
+++ spec/ttt/player_spec.clj
@@ -7,6 +7,9 @@
               (should= "X" (:marker (first players)))
               (should= "O" (:marker (second players))))
           (it "sets a current player"
-              (should= "X" (:marker (current-player players))))
+              (should= "X" (:marker (current-player players)))
+              (should= "O" (:marker (current-player (switch-players players)))))
           (it "gets player input"
-              (should= 1 (with-in-str "1" (get-move)))))
+              (should= 1 (with-in-str "1" (get-move))))
+          (it "switches players" 
+              (should= "O" (:marker (first (switch-players players))))))
--- src/ttt/player.clj
+++ src/ttt/player.clj
@@ -13,3 +13,9 @@
 (defn get-move
   []
   (read-string (read-line)))
+
+(defn switch-players
+  [[player-1 player-2]]
+  (if (= (:marker player-1) "X")
+    [player-2 player-1]
+    [player-1 player-2]))
