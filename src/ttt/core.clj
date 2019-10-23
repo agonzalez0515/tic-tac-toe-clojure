7110e4ee0ab511a944c5788142385615f7a9c6d2
Angelica Gonzalez <angelica@8thlight.com>
2019-10-23 11:04:16 -0700

    Add game state atom

--- src/ttt/core.clj
+++ src/ttt/core.clj
@@ -4,13 +4,28 @@
             [ttt.player :as player]
             [ttt.ui :as ui]))
 
+(def game-state (atom {:board (board/initial-board)
+                       :players player/players
+                       :current-player (first player/players)}))
+
+(defn set-game-state
+  [board players]
+  (swap! game-state 
+        (fn [current-state]
+           (merge current-state {:board board :current-player (first players)}))))
+
+(defn play 
+  []
+  (ui/print-board (:board @game-state))
+  (set-game-state 
+   (board/make-move (:board @game-state) (player/get-move) (:marker (:current-player @game-state))) 
+   (player/switch-players (:players @game-state)))
+  (ui/print-board (:board @game-state)))
+
+
 
 (defn -main
   [& args]
   (ui/start-game-message player/players)
-  (board/print-board (board/initial-board))
-  (board/make-move (board/initial-board) (player/get-move) "X"))
-
-
-
+  (play))
 
