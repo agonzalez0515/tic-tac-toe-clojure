10e53b10bf581a29993d7daa4852941daa8f92ea
Angelica Gonzalez <angelica@8thlight.com>
2019-10-28 16:02:18 -0700

    create tie? function and tests

--- spec/ttt/game_rules_spec.clj
+++ spec/ttt/game_rules_spec.clj
@@ -6,30 +6,48 @@
 (describe "Game Rules"
   (context "#win?"
     (it "returns false if there is no winner"
-      (should= false (rules/win? ["X","X", "O"
-                            "O","O", "X"
-                             6,  7,  "X"])))
+      (should= false (rules/win? ["X" "X"  "O"
+                                  "O" "O"  "X"
+                                   6   7   "X"])))
     (it "checks if the game is won for a row of X's"
-        (should= true (rules/win? ["X","X","X"
-                             "O", 4, "O"
-                              5,  6,  7 ])))
+      (should= true (rules/win? ["X" "X" "X"
+                                 "O"  4  "O"
+                                  5   6   7])))
     (it "checks if the game is won for a row of O's"
-        (should= true (rules/win? ["X", 1, "X"
-                             "O","O","O"
-                              5,  6,  7])))
+      (should= true (rules/win? ["X"  1  "X"
+                                 "O" "O" "O"
+                                  5   6   7])))
     (it "checks if the game is won for a column of X's"
-        (should= true (rules/win? ["X","O","O"
-                            "X", 4, "O"
-                            "X", 6,  7])))
+      (should= true (rules/win? ["X" "O" "O"
+                                 "X"  4  "O"
+                                 "X"  6   7])))
     (it "checks if the game is won for a column of O's"
-        (should= true (rules/win? ["X","O","X"
-                              "X","O","O"
-                              5, "O", 7])))
+      (should= true (rules/win? ["X" "O" "X"
+                                 "X" "O" "O"
+                                  5  "O"  7])))
     (it "checks if the game is won for a diagonal of X's"
-        (should= true (rules/win? ["O","X","X"
-                            "O","X","O"
-                            "X", 6,  7])))
+      (should= true (rules/win? ["O" "X" "X"
+                                 "O" "X" "O"
+                                 "X"  6   7])))
     (it "checks if the game is won for a diagonal of O's"
-        (should= true (rules/win? ["O","X","X"
-                              3, "O","O"
-                            "X", 6, "O"])))))
+      (should= true (rules/win? ["O" "X" "X"
+                                  3  "O" "O"
+                                 "X"  6  "O"]))))
+  (context "#tie?"
+    (it "returns true if the game is tied"
+      (should= true (rules/tie? ["O" "X" "O"
+                                 "O" "X" "X"
+                                 "X" "O" "X"])))
+    (it "returns false if the game is not tied"
+      (should= false (rules/tie? ["O" 1 "O"
+                                  "O" "X" "X"
+                                  "X" "O" "X"]))))
+  (context "#game-over?" 
+    (it "returns true if the game is over"
+      (should= true (rules/win? ["O" "X" "X"
+                                  3  "O" "O"
+                                 "X"  6  "O"])))
+    (it "returns false if the game is not over"
+      (should= false (rules/tie? ["O" 1 "O"
+                                  "O" "X" "X"
+                                  "X" "O" "X"])))))
--- src/ttt/game_rules.clj
+++ src/ttt/game_rules.clj
@@ -40,6 +40,18 @@
   [cells]
   (map (fn[collection] (= 1 (count (set collection)))) cells))
 
+(defn- available-spaces?
+  [board]
+  (boolean (some true? (map number? board))))
+
 (defn win?
   [board]
   (boolean (some true? (flatten (map check-group-of-cells (create-groups-of-cells board))))))
+
+(defn tie?
+  [board]
+  (and (not (available-spaces? board)) (not (win? board))))
+
+(defn game-over?
+  [board]
+  (or (win? board) (tie? board)))
