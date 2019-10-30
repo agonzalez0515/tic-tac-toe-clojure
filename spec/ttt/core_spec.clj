18686614dd241cc1abd651ef35fbb2d6163cdcf7
Angelica Gonzalez <angelica@8thlight.com>
2019-10-30 14:32:13 -0700

    Refactor game state to not use an atom. Create a new game state map after each loop

--- spec/ttt/core_spec.clj
+++ spec/ttt/core_spec.clj
@@ -2,13 +2,15 @@
   (:require [speclj.core :refer [describe context it should=]]
             [ttt.core :as game]))
 
-
+(def current-state {:board [0 1 2 3 4 5 6 7 8 ] 
+                    :players [{:marker "X"} {:marker "O"}] 
+                    :current-player {:marker "X"}})
 
 (describe "Game"
   (context "#set-game-state"
-    (it "has a game state"
-      (should= {:board [0 1 2 3 4 5 6 7 8], :players [{:marker "X"} {:marker "O"}] :current-player {:marker "X"}}
-        (game/set-game-state [0 1 2 3 4 5 6 7 8] [{:marker "X"} {:marker "O"}])))))
+    (it "sets a new game state"
+      (should= {:board ["X" 1 2 3 4 5 6 7 8], :players [{:marker "X"} {:marker "O"}] :current-player {:marker "X"}}
+        (game/set-game-state current-state ["X" 1 2 3 4 5 6 7 8] [{:marker "X"} {:marker "O"}])))))
 
 
 
--- src/ttt/core.clj
+++ src/ttt/core.clj
@@ -5,25 +5,25 @@
             [ttt.ui :as ui]
             [ttt.game-rules :as rules]))
 
-(def game-state (atom {:board (board/create-initial-board)
-                       :players player/players
-                       :current-player (first player/players)}))
+
+(def game-state {:board (board/create-initial-board)
+                :players player/players
+                :current-player (first player/players)})
 
 (defn set-game-state
-  [board players]
-  (swap! game-state 
-        (fn [current-state]
-           (merge current-state {:board board :players players :current-player (first players)}))))
+  [state board players]
+  (let [new-state {:board board :players players :current-player (first players)}]
+    (merge state new-state)))
 
 (defn play
-  []
-  (loop [state @game-state]
+  [starting-game-sate]
+  (loop [state starting-game-sate]
     (ui/print-board (:board state))
   (if (rules/game-over? (:board state))
     (println "game over")
     (let [next-board (board/make-move (:board state) (player/get-move) (:marker (:current-player state)))
           next-player (player/switch-players (:players state))]
-      (recur (set-game-state next-board next-player ))))))
+      (recur (set-game-state state next-board next-player ))))))
 
 
 
@@ -31,5 +31,5 @@
 (defn -main
   []
   (ui/print-start-game-message player/players)
-  (play))
+  (play game-state))
 
