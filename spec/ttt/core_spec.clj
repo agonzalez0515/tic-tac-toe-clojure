25a07e3ab57bcf9d17430aa3d84e82368579575d
Angelica Gonzalez <angelica@8thlight.com>
2019-11-02 21:55:52 -0700

    Clean up variable names

--- spec/ttt/core_spec.clj
+++ spec/ttt/core_spec.clj
@@ -6,8 +6,10 @@
             [ttt.board :as board]
             [ttt.player :as player]))
 
-(def current-state {:board [0 1 2 3 4 5 6 7 8] 
-                    :players [{:marker "X"} {:marker "O"}] 
+(def players [{:marker "O"} {:marker "X"}])
+
+(def initial-state {:board [0 1 2 3 4 5 6 7 8]
+                    :players [{:marker "X"} {:marker "O"}]
                     :current-player {:marker "X"}})
 
 (def new-state {:board [0 1 2 3 "X" 5 6 7 8]
@@ -18,42 +20,40 @@
                             :players [{:marker "X"} {:marker "O"}]
                             :current-player {:marker "X"}})
 
-(def players [{:marker "O"} {:marker "X"}])
+(def boards-printed-after-one-move "\n0 | 1 | 2\n---------\n3 | 4 | 5\n---------\n6 | 7 | 8\n\n0 | X | 2\n---------\n3 | 4 | 5\n---------\n6 | 7 | 8\nGame over\n")
+(def boards-printed-after-two-moves "\n0 | 1 | 2\n---------\n3 | 4 | 5\n---------\n6 | 7 | 8\n\n0 | 1 | 2\n---------\n3 | X | 5\n---------\n6 | 7 | 8\n\n0 | 1 | 2\n---------\n3 | X | 5\n---------\n6 | 7 | O\nGame over\n")
 
 
-(describe "Game"          
-  (context "#play"
-    (with-stubs)
-    
-    (it "calls game over message when the game is over"
-      (with-redefs [rules/game-over? (stub :rules/game-over? {:return true})]
-        
-      (should-invoke ui/print-game-over-message {:with []} (game/play current-state))))     
-    
-    (it "makes a move when the game is not over"
-      (with-redefs [player/get-move (stub :player/get-move {:return 4})
-                    game/set-game-state (stub :game/set-game-state {:return new-state})
-                    rules/game-over? (let [results (atom [false true])]
-                                        (fn [_] (ffirst (swap-vals! results rest))))]
+(describe "Game"
+          (context "#play"
+            (with-stubs)
 
-      (should-invoke board/make-move {:with [[0 1 2 3 4 5 6 7 8] 4 "X"]} (game/play current-state))))
-    
-    (it "prints the new board after a move is made"
-      (with-redefs [player/get-move (stub :player/get-move {:return 1})
-                    board/make-move (stub :board/make-move {:return [0 "X" 2 3 4 5 6 7 8]})
-                    rules/game-over? (let [results (atom [false true])]
-                                        (fn [_] (ffirst (swap-vals! results rest))))]
-        
-      (should= "\n0 | 1 | 2\n---------\n3 | 4 | 5\n---------\n6 | 7 | 8\n\n0 | X | 2\n---------\n3 | 4 | 5\n---------\n6 | 7 | 8\nGame over\n" 
-               (with-out-str (game/play current-state)))))
-    
-    (it "prints new boards after each player makes a move"
-      (with-redefs [player/get-move (let [results (atom [4 8])]
-                                      (fn [] (ffirst (swap-vals! results rest))))
-                    board/make-move (let [results (atom [[0 1 2 3 "X" 5 6 7 8] [0 1 2 3 "X" 5 6 7 "O"]])]
-                                      (fn [_a _b _c] (ffirst (swap-vals! results rest))))
-                    rules/game-over? (let [results (atom [false false true])]
-                                        (fn [_] (ffirst (swap-vals! results rest))))]
+            (it "calls game over message when the game is over"
+                (with-redefs [rules/game-over? (stub :rules/game-over? {:return true})]
+                  (should-invoke ui/print-game-over-message {:with []} (game/play initial-state))))
 
-      (should= "\n0 | 1 | 2\n---------\n3 | 4 | 5\n---------\n6 | 7 | 8\n\n0 | 1 | 2\n---------\n3 | X | 5\n---------\n6 | 7 | 8\n\n0 | 1 | 2\n---------\n3 | X | 5\n---------\n6 | 7 | O\nGame over\n"
-                (with-out-str (game/play current-state)))))))
+            (it "makes a move when the game is not over"
+                (with-redefs [player/get-move (stub :player/get-move {:return 4})
+                              game/set-game-state (stub :game/set-game-state {:return new-state})
+                              rules/game-over? (let [results (atom [false true])]
+                                                 (fn [_] (ffirst (swap-vals! results rest))))]
+
+                  (should-invoke board/make-move {:with [[0 1 2 3 4 5 6 7 8] 4 "X"]} (game/play initial-state))))
+
+            (it "prints the new board after a move is made"
+                (with-redefs [player/get-move (stub :player/get-move {:return 1})
+                              board/make-move (stub :board/make-move {:return [0 "X" 2 3 4 5 6 7 8]})
+                              rules/game-over? (let [results (atom [false true])]
+                                                 (fn [_] (ffirst (swap-vals! results rest))))]
+
+                  (should= boards-printed-after-one-move (with-out-str (game/play initial-state)))))
+
+            (it "prints new boards after each player makes a move"
+                (with-redefs [player/get-move (let [results (atom [4 8])]
+                                                (fn [] (ffirst (swap-vals! results rest))))
+                              board/make-move (let [results (atom [[0 1 2 3 "X" 5 6 7 8] [0 1 2 3 "X" 5 6 7 "O"]])]
+                                                (fn [_a _b _c] (ffirst (swap-vals! results rest))))
+                              rules/game-over? (let [results (atom [false false true])]
+                                                 (fn [_] (ffirst (swap-vals! results rest))))]
+
+                  (should= boards-printed-after-two-moves (with-out-str (game/play initial-state)))))))
--- src/ttt/core.clj
+++ src/ttt/core.clj
@@ -5,30 +5,28 @@
             [ttt.ui :as ui]
             [ttt.game-rules :as rules]))
 
-(def game-state {:board (board/create-initial-board)
-                :players player/players
-                :current-player (first player/players)})
+(def initial-state {:board (board/create-initial-board)
+                    :players player/players
+                    :current-player (first player/players)})
 
 (defn- set-game-state
   [state board players]
   (assoc state :board board :players players :current-player (first players)))
 
 (defn play
-  [starting-game-state]
-  (loop [state starting-game-state]
+  [initial-state]
+  (loop [state initial-state]
     (ui/print-board (:board state))
   (if (rules/game-over? (:board state))
     (ui/print-game-over-message)
-    (let [next-board (board/make-move 
-                      (:board state) 
-                      (player/get-move) 
-                      (:marker (:current-player state)))
+    (let [next-board (board/make-move
+                       (:board state)
+                       (player/get-move)
+                       (:marker (:current-player state)))
           next-player (player/switch-players (:players state))]
       (recur (set-game-state state next-board next-player ))))))
 
 (defn -main
   []
   (ui/print-start-game-message player/players)
-  (play game-state))
-
-
+  (play initial-state))
