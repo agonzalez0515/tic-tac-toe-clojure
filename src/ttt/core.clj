961108c7658cf04c33afb01df13f7203b80eea3c
Angelica Gonzalez <angelica@8thlight.com>
2019-11-07 13:18:49 -0800

    Add functions to save stats to database

--- src/ttt/core.clj
+++ src/ttt/core.clj
@@ -4,32 +4,54 @@
             [ttt.player :as player]
             [ttt.ui :as ui]
             [ttt.game-rules :as rules]
-            [database :as db ]))
+            [ttt.game-setup :as setup]
+            [database :as db]))
 
 (def initial-state {:board (board/create-initial-board)
-                    :players player/players
-                    :current-player (first player/players)})
+                    :players  (setup/setup-players player/players)})
 
 (defn- set-game-state
   [state board players]
   (assoc state :board board :players players :current-player (first players)))
 
+
+(defn set-win-stats
+  [winner loser]
+  (db/update-stats-when-winner winner loser)
+  (ui/print-win-message winner)
+  (ui/print-stats (db/retrieve-all-player-stats [winner loser])))
+
+(defn set-ties
+  [player1 player2]
+  (db/update-stats-when-tie player1 player2)
+  (ui/print-tie-message)
+  (ui/print-stats (db/retrieve-all-player-stats [player1 player2])))
+
+(defn- player2
+  [players]
+  (:username (second players)))
+
+(defn- player1
+  [players]
+  (:username (first players)))
+
 (defn play
   [initial-state]
-  (loop [state initial-state]
+  (loop [state (assoc initial-state :current-player (first (:players initial-state)))]
     (ui/print-board (:board state))
-  (if (rules/game-over? (:board state))
-    (ui/print-game-over-message)
-    (let [next-board (board/make-move
-                       (:board state)
-                       (player/get-move)
-                       (:marker (:current-player state)))
-          next-player (player/switch-players (:players state))]
-      (recur (set-game-state state next-board next-player ))))))
+    (ui/print-player-instructions (:username (:current-player state)))
+    (cond 
+      (rules/win? (:board state)) (set-win-stats (player2 (:players state)) (player1 (:players state)))
+      (rules/tie? (:board state)) (set-ties (player1 (:players state)) (player2 (:players state)))
+      :else (let [next-board (board/make-move
+                        (:board state)
+                        (player/get-move)
+                        (:marker (:current-player state)))
+            next-player (player/switch-players (:players state))]
+        (recur (set-game-state state next-board next-player))))))
 
 
 (defn -main
   []
-  (ui/print-start-game-message player/players)
-  
+  (ui/print-start-game-message)
   (play initial-state))
