c90f3ec9a47e1f8891975431208328dbd9605757
Angelica Gonzalez <angelica@8thlight.com>
2019-11-07 13:19:48 -0800

    Set up players with usernames

--- spec/ttt/game_setup_spec.clj
+++ spec/ttt/game_setup_spec.clj
@@ -1,12 +1,35 @@
 (ns ttt.game-setup-spec
-  (:require [speclj.core :refer [context describe it should=]]
+  (:require [speclj.core :refer [context should-invoke describe it should= stub with-stubs]]
             [ttt.game-setup :as setup]
-            [ttt.ui :as ui]))
+            [ttt.ui :as ui]
+            [database :as db]))
 
 (describe "Game Setup"
-  (it "creates a list of players"
-      (with-redefs [ui/get-player-username (let [results (atom ["Angie" "Jon"])]
-                                             (fn [_] (ffirst (swap-vals! results rest))))]
-    (should= ["Angie" "Jon"] (setup/get-players [{:marker "X"} {:marker "O"}]))))
+  (with-stubs)
   
-  (it ))
+  (context "#get-usernames"
+    (it "creates a list of players"
+      (with-redefs [ui/get-player-username (let [results (atom ["Angie" "Jon"])]
+                                              (fn [_] (ffirst (swap-vals! results rest))))]
+        (should= ["Angie" "Jon"] (setup/get-usernames [{:marker "X"} {:marker "O"}])))))
+  
+  (context "#setup-players"
+    (it "saves a username" 
+      (with-redefs [db/save-username (stub :db/save-username {:return []})
+                    setup/get-usernames (stub :setup/get-usernames {:return ["Angie"]})]
+        (should-invoke db/save-username {:with ["Angie"]} (setup/setup-players [{:marker "X"} {:marker "O"}] ))))
+            
+    (it "saves a set of usernames"
+      (with-redefs [db/save-username (stub :db/save-username {:return []})
+                    setup/get-usernames (stub :setup/get-usernames {:return ["Angie" "Jon"]})]
+        (should-invoke db/save-username {:with ["Jon"]} (setup/setup-players [{:marker "X"} {:marker "O"}]))))
+           
+    (it "creates a player with a username"
+        (with-redefs [db/save-username (stub :db/save-username {:return []})
+                      setup/get-usernames (stub :setup/get-usernames {:return ["Angie" "Jon"]})]
+          (should-invoke db/save-username {:with ["Jon"]} (setup/setup-players [{:marker "X"} {:marker "O"}]))))
+    
+    (it "returns players with usernames"
+        (with-redefs [db/save-username (stub :db/save-username {:return []})
+                      setup/get-usernames (stub :setup/get-usernames {:return ["Angie" "Jon"]})]
+          (should= '({:username "Angie", :marker "X"} {:username "Jon", :marker "O"}) (setup/setup-players [{:marker "X"} {:marker "O"}]))))))
--- src/ttt/game_setup.clj
+++ src/ttt/game_setup.clj
@@ -1,8 +1,9 @@
 (ns ttt.game-setup
-  (:require [ttt.ui :as ui]))
+  (:require [ttt.ui :as ui]
+            [database :as db]))
 
 
-(defn get-players
+(defn get-usernames
   [players]
   (map ui/get-player-username players))
   
@@ -10,6 +11,12 @@
   [player]
   (db/save-username player))
 
+(defn- create-usernames-hash
+  [names]
+  (mapv (partial zipmap [:username :username]) (into [] (map vec (partition 1 names)))))
+
 (defn setup-players 
   [players]
-  (doseq [player (get-players players)] (save-username player)))
+  (let [usernames (get-usernames players)]
+    (doseq [player usernames] (save-username player))
+    (map merge (create-usernames-hash usernames) players)))
